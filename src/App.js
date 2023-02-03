1b30dd53fc926395e677c2d706b568095a844dc7
Dr Vicki Bealman <drvicki.bealman@gmail.com>
2023-02-03 09:53:18 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>
+          Welcome to Final Project!
+        </h1>
+        <h3> Start coding with Dr. Vicki to create your own Google Clone!</h3>
+        <h5>FOLLOW INSTRUCTIONS VERY CAREFULLY</h5>
+        
       </header>
     </div>
   );
